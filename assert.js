d468756f403898365e9f90ac2ebd7a3b660169de
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-04-11 11:29:27 +0530

    assert example

---
+++ assert.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+
+const obj1 = {
+  a: {
+    b: 1
+  }
+};
+const obj2 = {
+  a: {
+    b: 2
+  }
+};
+const obj3 = {
+  a: {
+    b: 1
+  }
+};
+const obj4 = Object.create(obj1);
+
+assert.deepEqual(obj1, obj1);
+// OK, object is equal to itself
+
+assert.deepEqual(obj1, obj2);
+// AssertionError: { a: { b: 1 } } deepEqual { a: { b: 2 } }
+// values of b are different
+
+assert.deepEqual(obj1, obj3);
+// OK, objects are equal
+
+assert.deepEqual(obj1, obj4);
+// AssertionError: { a: { b: 1 } } deepEqual {}
+// Prototypes are ignored
+
+const assert = require('assert');
+
+assert.deepEqual({ a: 1 }, { a: '1' });
+// OK, because 1 == '1'
+
+assert.deepStrictEqual({ a: 1 }, { a: '1' });
+// AssertionError: { a: 1 } deepStrictEqual { a: '1' }
+// because 1 !== '1' using strict equality
+
+// The following objects don't have own properties
+const date = new Date();
+const object = {};
+const fakeDate = {};
+
+Object.setPrototypeOf(fakeDate, Date.prototype);
+
+assert.deepEqual(object, fakeDate);
+// OK, doesn't check [[Prototype]]
+assert.deepStrictEqual(object, fakeDate);
+// AssertionError: {} deepStrictEqual Date {}
+// Different [[Prototype]]
+
+assert.deepEqual(date, fakeDate);
+// OK, doesn't check type tags
+assert.deepStrictEqual(date, fakeDate);
+// AssertionError: 2017-03-11T14:25:31.849Z deepStrictEqual Date {}
+// Different type tags
+
+assert.deepStrictEqual(new Number(1), new Number(2));
+// Fails because the wrapped number is unwrapped and compared as well.
+assert.deepStrictEqual(new String('foo'), Object('foo'));
+// OK because the object and the string are identical when unwrapped.
+
+
+const async_hooks = require('async_hooks');
+
+console.log(async_hooks.executionAsyncId());  // 1 - bootstrap
+fs.open(path, 'r', (err, fd) => {
+  console.log(async_hooks.executionAsyncId());  // 6 - open()
+});
